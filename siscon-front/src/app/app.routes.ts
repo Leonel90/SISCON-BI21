35247bf7e9b1f83092c353ce6c80b188bc88b549
Leonel90 <sleonel1990@gmail.com>
2025-02-24 13:16:22 -0500

    Enrutamiento
    ruta principal y rutas hijas y default

--- siscon-front/src/app/app.routes.ts
+++ siscon-front/src/app/app.routes.ts
@@ -3,20 +3,27 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     /* Rutas Publicas*/
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.routes'),
+    path: '',
+    loadComponent: () => import('./share/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+      },
+      {
+        path: 'pages',
+        loadChildren: () => import('./pages/pages.routes'),
+      },
+      {
+        /*Rutas Privadas */
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
-    path: 'pages',
-    loadChildren: () => import('./pages/pages.routes'),
-  },
-  {
-    /*Rutas Privadas */
-    path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.component'),
-  }/*,
-  {
-    path: '**',
-    redirectTo: 'dashboard',
-  },*/
+      path: '**',
+      redirectTo: 'dashboard',
+  }
 ];
